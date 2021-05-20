44dc161bd4ef087c439f5f44fefa540e0f40fc93
Nguyen Viet Thai <vietthai2512@gmail.com>
2021-05-20 13:17:23 +0700

    Update models

---
+++ src/main/java/com/zeroToHero/FinalProject/models/Blogs.java
@@ -0,0 +1,67 @@
+package com.zeroToHero.FinalProject.models;
+
+
+public class Blogs {
+
+    private long blogId;
+    private String userId;
+    private java.sql.Timestamp createdAt;
+    private java.sql.Timestamp updatedAt;
+    private String content;
+    private String images;
+
+
+    public long getBlogId() {
+        return blogId;
+    }
+
+    public void setBlogId(long blogId) {
+        this.blogId = blogId;
+    }
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+
+    public java.sql.Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(java.sql.Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+
+    public java.sql.Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(java.sql.Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+    public String getImages() {
+        return images;
+    }
+
+    public void setImages(String images) {
+        this.images = images;
+    }
+
+}
