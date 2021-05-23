17256398a135e8cc7d7073d602781c9c484d9176
Nguyễn Việt Thái <vietthai2512@gmail.com>
2021-05-23 22:08:56 +0700

    Update HomeServlet.java

--- src/main/java/com/zeroToHero/FinalProject/servlets/HomeServlet.java
+++ src/main/java/com/zeroToHero/FinalProject/servlets/HomeServlet.java
@@ -16,22 +16,9 @@ public class HomeServlet extends HttpServlet
 {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-<<<<<<< HEAD
-        ArrayList<Tours> temp = null;
-        String error = "1";
-        try {
-            temp = (ArrayList<Tours>) new HomeController().getPopularDestinations();
-        } catch (SQLException throwable) {
-            error = throwable.getMessage();
-            throwable.printStackTrace();
-        }
-        req.setAttribute("error",error);
-        req.setAttribute("result", temp);
-=======
         ArrayList<Tours> popTours = new ToursDAO().getPopularTours();
 
         req.setAttribute("popTours", popTours);
->>>>>>> ab5c521107a01b3f7735ee8b561266e3e89ed0cc
         req.getRequestDispatcher("/WEB-INF/views/home.jsp").forward(req, resp);
     }
 }
