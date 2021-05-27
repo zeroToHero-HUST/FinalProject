9a0486fd74f6cd913018ceaa15e76c705b29ac81
Nguyen Viet Thai <vietthai2512@gmail.com>
2021-05-27 13:00:48 +0700

    Add bookings list in admin

--- src/main/java/com/zeroToHero/FinalProject/models/dao/BookingsDAO.java
+++ src/main/java/com/zeroToHero/FinalProject/models/dao/BookingsDAO.java
@@ -71,4 +71,34 @@ public class BookingsDAO {
 
         return result;
     }
+
+    public ArrayList<Bookings> getAllBookings()
+    {
+        ArrayList<Bookings> result = new ArrayList<>();
+        try {
+            conn = DBConnectionManager.getConnection();
+            pst = conn.prepareStatement(BookingsQuery.getAllBookings);
+            rs = pst.executeQuery();
+
+            while (rs.next())
+            {
+                Bookings temp = new Bookings();
+                temp.setBookingId(rs.getLong("booking_id"));
+                temp.setTourTitle(rs.getString("title"));
+                temp.setUserId(rs.getString("user_id"));
+                temp.setCreatedAt(rs.getTimestamp("created_at"));
+                temp.setStartDate(rs.getDate("start_date"));
+                temp.setEndDate(rs.getDate("end_date"));
+                temp.setPrice(rs.getString("price"));
+
+                result.add(temp);
+            }
+        } catch (SQLException | NamingException e) {
+            e.printStackTrace();
+        } finally {
+            DBConnectionManager.closeConnection(conn, pst, rs);
+        }
+
+        return result;
+    }
 }
